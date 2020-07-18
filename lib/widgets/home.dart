f09eadb69cebba9dfc8f9afa7c1dcf8c108f4297
Debjit Chakraborty <zxyron0@gmail.com>
2020-07-18 08:10:29 +0530

    Separate the card widgets

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:instaAd/models/peopledata.dart';
 import 'package:instaAd/utils/variables.dart';
+
+import 'cardwidgets.dart';
 
 class HomeList extends StatefulWidget {
   @override
@@ -8,12 +9,9 @@ class HomeList extends StatefulWidget {
 }
 
 class _HomeListState extends State<HomeList> {
-  var acc;
-
   @override
   void initState() {
     super.initState();
-    // acc = getAccounts();
   }
 
   @override
@@ -47,101 +45,6 @@ class _HomeListState extends State<HomeList> {
           );
         }
       },
-    );
-  }
-}
-
-class DetailWidget extends StatelessWidget {
-  const DetailWidget({
-    Key key,
-    @required this.likes,
-    @required this.followers,
-    @required this.messages,
-    @required this.posts,
-    @required this.url,
-    @required this.name,
-  }) : super(key: key);
-
-  final String likes;
-  final String followers;
-  final String messages;
-  final String posts;
-  final String url;
-  final String name;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      child: Row(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                radius: 30.0,
-                backgroundImage: NetworkImage(url),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(name),
-            ],
-          ),
-          Spacer(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CardInfo(
-                icon: Icons.photo_library,
-                text: posts,
-              ),
-              SizedBox(height: 10.0),
-              CardInfo(
-                icon: Icons.message,
-                text: messages,
-              ),
-            ],
-          ),
-          SizedBox(width: 50.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CardInfo(
-                icon: Icons.account_circle,
-                text: followers,
-              ),
-              SizedBox(height: 10.0),
-              CardInfo(
-                icon: Icons.favorite,
-                text: likes,
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CardInfo extends StatelessWidget {
-  const CardInfo({
-    Key key,
-    @required this.icon,
-    @required this.text,
-  }) : super(key: key);
-
-  final icon;
-  final text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(icon),
-        SizedBox(width: 10.0),
-        Text(text),
-      ],
     );
   }
 }
